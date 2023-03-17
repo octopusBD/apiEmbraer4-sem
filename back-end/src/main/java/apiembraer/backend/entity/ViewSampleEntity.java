54be7039e3d64b2e4e16dd395837b2dc5f54d8fc
Tiago Camillo <66873418+tcamillo4-ctrlz@users.noreply.github.com>
2023-03-17 20:15:32 -0300

    FI - 31
    Realizado o mapeamento da view Sample no banco de dados do projeto utilizando Spring Boot.

---
+++ back-end/src/main/java/apiembraer/backend/entity/ViewSampleEntity.java
@@ -0,0 +1,28 @@
+package apiembraer.backend.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Immutable;
+
+import lombok.Data;
+
+@Entity
+@Immutable
+@Data
+@Table(name = "View_Sample")
+public class ViewSampleEntity {
+
+	@Column(name = "CHASSI")
+	private String chassi;
+	
+	@Column(name = "BOLETIM")
+	private String boletim;
+	
+	@Id
+	@Column(name = "STATUS_SAMPLE")
+	private String status_sample;
+	
+}
