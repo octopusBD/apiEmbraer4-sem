16c0051e13014d64c52405558af51517fc4845a2
Michael <michaelfelipe180@gmail.com>
2023-03-24 19:24:45 -0300

    Atualizacao footer

---
+++ front-end-acc/src/components/SelecaoInput.vue
@@ -0,0 +1,33 @@
+<template>
+    <!-- #ajustar label -->
+    <div class="select">
+        <v-select
+        label=""
+        :items=itens
+        density="compact"
+        bg-color="white"
+        hide-selected
+
+        ></v-select>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        itens: [
+        'California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming'
+        ],
+    }),
+    }
+    
+</script>
+
+<style>
+    .select{
+        height: 30px;
+        width: 200px;
+
+    }
+
+</style>
