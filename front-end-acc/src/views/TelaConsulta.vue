fbb3a958d15af0f4c85840a4610b929fcdd8113f
Ana Clara Oliveira Leal <90358925+heyanaleal@users.noreply.github.com>
2023-03-24 19:02:39 -0300

    paginacao e estilizacao da tabela

--- front-end-acc/src/views/TelaConsulta.vue
+++ front-end-acc/src/views/TelaConsulta.vue
@@ -1,11 +1,14 @@
 <template>
-    <HelloWorld />
-    <TabelaInput/>
+    <HeaderInput />
+    <TabelaInput />
+    <Footersrinput />
   </template>
   
   <script setup>
-    import HelloWorld from '@/components/HeaderInput.vue'
+    import HeaderInput from '@/components/HeaderInput.vue'
     import TabelaInput from '@/components/TabelaInput.vue';
+    import Footersrinput from '@/components/Footersrinput.vue';
 
+ 
   </script>
   
