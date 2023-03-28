79e4f2c819b0224ee6bd1524727e7d5eeb989123
Ana Clara Oliveira Leal <90358925+heyanaleal@users.noreply.github.com>
2023-03-28 19:05:29 -0300

    update notificação

---
+++ front-end-acc/src/components/NotificacaoInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-btn @click="openPopup" prepend-icon="mdi-bell" variant="text" color="white"></v-btn>
+    <v-dialog v-model="showPopup" max-width="500">
+      <v-card>
+        <v-card-title>Notificações</v-card-title>
+        <v-card-text>
+          <v-list>
+            <v-list-item v-for="notification in notifications" :key="notification.id">
+              <v-list-item-title>{{ notification.message }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      notifications: [
+        { id: 1, message: "Notificação 1" },
+        { id: 2, message: "Notificação 2" },
+        { id: 3, message: "Notificação 3" },
+      ],
+      showPopup: false,
+    };
+  },
+  methods: {
+    openPopup() {
+      this.showPopup = true;
+    },
+  },
+};
+</script>
