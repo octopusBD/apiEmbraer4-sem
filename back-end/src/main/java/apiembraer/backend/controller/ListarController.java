5ec72e1498704a0654f70c0cea3554da2ba49bd4
Aldrik Alvaro <aldrikfariaalvaro@gmail.com>
2023-03-31 14:48:57 -0300

    Revert "Merge branch 'main' of https://github.com/octopusBD/apiEmbraer4-sem into Sprint-1"
    This reverts commit 637177efecb64cb12f6c622fcfee64a13caef4fa, reversing
    changes made to 5d048eb5f17861a3c92b0c61fccb812bae80e5fd.

--- back-end/src/main/java/apiembraer/backend/controller/ListarController.java
+++ back-end/src/main/java/apiembraer/backend/controller/ListarController.java
@@ -10,19 +10,9 @@ import apiembraer.backend.service.ListarService;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import apiembraer.backend.entity.ViewSampleEntity;
-import apiembraer.backend.service.ListarService;
-
+;
 @CrossOrigin
-@Controller
+@RestController
 @RequestMapping(value = "/consultor")
 public class ListarController {
 
