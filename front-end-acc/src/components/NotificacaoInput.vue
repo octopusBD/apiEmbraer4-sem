5bb5eef5568ccfd2338f8c836c086368044997d9
Ana Clara Oliveira Leal <90358925+heyanaleal@users.noreply.github.com>
2023-03-31 17:00:12 -0300

    fechar notificação (botão)

--- front-end-acc/src/components/NotificacaoInput.vue
+++ front-end-acc/src/components/NotificacaoInput.vue
@@ -5,31 +5,32 @@
     </v-badge>
 
     <!-- Botão para abrir o popup de notificações -->
-    <v-btn class="text-none" stacked @click="openPopup" prepend-icon="mdi-bell" variant="text">
-    </v-btn>
-
+    <v-btn class="notification-button" @click="openPopup" prepend-icon="mdi-bell" variant="text"></v-btn>
     <!-- Popup de notificações -->
-    <v-dialog v-model="showPopup" max-width="600">
-      <v-card>
-        <div class="text-center ma-2">
-          <!-- Botão para abrir o snackbar -->
+   <v-dialog v-model="showPopup" max-width="600" >
+    <v-card class="notification-card">
+        <!-- <div class="text-center ma-2">
           <v-btn @click="openSnackbar">Open Snackbar</v-btn>
 
-          <!-- Snackbar para exibir mensagens ao usuário -->
           <v-snackbar v-model="snackbar" >
             {{ text }}
 
-            <!-- Botão para fechar o snackbar -->
             <template v-slot:actions>
               <v-btn color="blue" variant="text" @click="snackbar = false">Close</v-btn>
             </template>
           </v-snackbar>
-        </div>
-        <v-card-title>Notificações</v-card-title>
+        </div> -->
+        <v-card-title style="text-align: center; size: 30px; margin-top: 10px;">Notificações</v-card-title>
+        <v-divider></v-divider>
         <v-card-text>
           <v-list>
             <v-list-item v-for="(notification, index) in notifications" :key="index" @click="selectedNotification = notification; numNotifications--;">
-                            <v-list-item-title>{{ notification.mensagem }}</v-list-item-title>
+              <v-list-item-action>
+                <v-btn icon @click.stop="notifications.splice(index, 1) ,selectedNotification = notification; numNotifications--;">
+                  <v-icon>mdi-close</v-icon>
+                </v-btn>
+              </v-list-item-action>
+              <v-list-item-title>{{ notification.mensagem }}</v-list-item-title>
             </v-list-item>
           </v-list>
         </v-card-text>
@@ -96,5 +97,18 @@ export default {
   height: 100px;
   color: white;
   border-radius: 10rem;
+}
+notification-card {
+  background-color: #fff;
+  border-radius: 10px;
+  transition: all 0.3s ease-out;
+}
+
+.notification-button {
+  color: #fff;
+  border-radius: 100%;
+  height: 30px;
+  width: 30px;
+  font-size: 30px;
 }
 </style>
