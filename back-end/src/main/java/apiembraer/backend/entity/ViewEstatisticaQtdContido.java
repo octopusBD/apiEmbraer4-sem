b7aa577e68c14aaa9b02d95ce9f9bf3efcc0483a
Tiago Camillo <66873418+tcamillo4-ctrlz@users.noreply.github.com>
2023-04-17 19:34:41 -0300

    FI-109
    Feito o mapeamento da entidade Estatística Quantidade Contido

---
+++ back-end/src/main/java/apiembraer/backend/entity/ViewEstatisticaQtdContido.java
@@ -0,0 +1,34 @@
+package apiembraer.backend.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Immutable;
+
+import lombok.Data;
+
+@Entity
+@Immutable
+@Data
+@Table(name = "view_estatistica_qtd_contido")
+public class ViewEstatisticaQtdContido {
+
+	@Id
+	@Column(name = "ID_QTD_CONTIDO")
+	private Integer idQtdContido;
+	
+	@Column(name = "ID_CHASSI")
+	private Integer idChassi;
+	
+	@Column(name = "CHASSI")
+	private String chassi;
+	
+	@Column(name = "STATUS")
+	private String status;
+
+	@Column(name = "QTD_CONTIDO")
+	private Integer qtdBoletins;
+	
+}
