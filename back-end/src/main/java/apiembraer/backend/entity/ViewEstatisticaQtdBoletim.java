497da95b4aa0b9f3aa0cc2d68866978c2dc20c6a
Tiago Camillo <66873418+tcamillo4-ctrlz@users.noreply.github.com>
2023-04-17 19:31:04 -0300

    FI-108
    Feito o mapeamento da entidade Estatística Quantidade Boletim

---
+++ back-end/src/main/java/apiembraer/backend/entity/ViewEstatisticaQtdBoletim.java
@@ -0,0 +1,31 @@
+package apiembraer.backend.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Immutable;
+
+import lombok.Data;
+
+@Entity
+@Immutable
+@Data
+@Table(name = "view_estatistica_qtd_boletim")
+public class ViewEstatisticaQtdBoletim {
+
+	@Id
+	@Column(name = "ID_QTD_BOLETIM")
+	private Integer idQtdBoletim;
+	
+	@Column(name = "ID_CHASSI")
+	private Integer idChassi;
+	
+	@Column(name = "CHASSI")
+	private String chassi;
+
+	@Column(name = "QTD_BOLETINS")
+	private Integer qtdBoletins;
+	
+}
