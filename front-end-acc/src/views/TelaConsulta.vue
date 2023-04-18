ee6e7fc984c4b150106d37b25e750bb96423963e
Ana Clara Oliveira Leal <90358925+heyanaleal@users.noreply.github.com>
2023-04-18 20:50:06 -0300

    update

--- front-end-acc/src/views/TelaConsulta.vue
+++ front-end-acc/src/views/TelaConsulta.vue
@@ -1,34 +1,11 @@
 <template>
-  <v-card>
-    <v-card-title>Meu Gráfico</v-card-title>
-    <v-card-text>
-      <Bar :data="chartData" />
-    </v-card-text>
-  </v-card>
-</template>
-
-<script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
-export default {
-  name: 'BarChart',
-  components: { Bar },
-  data() {
-    return {
-      chartData: {
-        labels: [ 'January', 'February', 'March'],
-        datasets: [
-          {
-            label: 'Data One',
-            backgroundColor: '#f87979',
-            data: [40, 20, 12]
-          }
-        ]
-      }
-    }
-  }
-}
-</script>
+    <HeaderInput />
+    <TabelaInput />
+    <Footersrinput />
+  </template>
+  
+  <script setup>
+    import HeaderInput from '@/components/HeaderInput.vue'
+    import TabelaInput from '@/components/TabelaInput.vue'
+    import Footersrinput from '@/components/Footersrinput.vue'
+  </script>
