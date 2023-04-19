5c1bb422b585ebb111d8d7a8edec313f59e1eca6
ecampos14 <88864112+ecampos14@users.noreply.github.com>
2023-04-18 21:19:36 -0300

    FI-96

---
+++ back-end/src/main/java/apiembraer/backend/DTO/UsuarioDTO.java
@@ -0,0 +1,17 @@
+package apiembraer.backend.DTO;
+
+import apiembraer.backend.entity.PermissaoEntity;
+import lombok.Data;
+
+@Data
+public class UsuarioDTO {
+	
+
+	private Integer idUsuario;
+	private String nomeUsuario;
+	private String loginUsuario;
+	private String senhaUsuario;
+	private String email;
+	private PermissaoEntity idPermissao;
+
+}
