e6440a660c181bd0bc8c03849ae55a50abe20dd8
Aldrik Alvaro <aldrikfariaalvaro@gmail.com>
2023-04-19 22:16:20 -0300

    Update GraficoBar.vue

--- front-end-acc/src/components/GraficoBar.vue
+++ front-end-acc/src/components/GraficoBar.vue
@@ -9,64 +9,81 @@
 
 <script>
 import Chart from 'chart.js/auto';
+import axios from 'axios';
 import { onMounted, ref } from 'vue';
-
-const labels = [
-  "January",
-  "February",
-  'abril', 
-  'maio'
-];
-
-const data = {
-  labels: labels,
-  datasets: [{
-    label: "My Dataset",
-    borderColor: 'rgb(255, 99, 132)',
-    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-    data: [0, 1, 2, 10, 20, 14, 15]
-  }]
-};
-
-const config = {
-  type: 'bar',
-  data: data,
-  options: {
-    plugins: {
-      zoom: {
-        zoom: {
-          drag: {
-            selection: true
-          },
-          mode: 'xy'
-        },
-        onZoomComplete: function ({ chart }) {
-          console.log('Zoom complete', chart.scales);
-        }
-      }
-    }
-  }
-};
 
 export default {
   setup() {
     const chartCanvas = ref(null);
-    const chart = ref(null);
+    const qtdBoletins = ref([]);
+    const chassi = ref([]);
+    const labels = ref([]);
 
-    onMounted(() => {
-      if (chartCanvas.value) {
-        const ctx = chartCanvas.value.getContext('2d');
-        if (ctx) {
-          chart.value = new Chart(ctx, config);
+    const getData = async () => {
+  try {
+    const response = await axios.get("/estatistica/listar/boletim");
+    chassi.value = response.data.map((item) => item.chassi);
+    qtdBoletins.value = response.data.map((item) => item.qtdBoletins);
+
+    console.log(response);
+    
+  } catch (error) {
+    console.log(error);
+  }
+  createChart();
+};
+const createChart = () => {
+  if (!chartCanvas.value) return;
+
+  const data = {
+    labels: chassi.value,
+    datasets: [
+      
+      {
+        label: "chassi",
+        borderColor: 'rgb(54, 162, 235)',
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        data: qtdBoletins.value // Alteração aqui
+      }
+    ]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      },
+      plugins: {
+        zoom: {
+          zoom: {
+            drag: {
+              selection: true
+            },
+            mode: 'xy'
+          },
+          onZoomComplete: function ({ chart }) {
+            console.log('Zoom complete', chart.scales);
+          }
         }
       }
+    }
+  };
+
+  chartCanvas.value = new Chart(chartCanvas.value, config);
+};
+    onMounted(() => {
+      getData();
     });
 
-    return { chartCanvas };
-  },
+    return {
+      chartCanvas
+    };
+  }
 };
 </script>
-
-<style>
-
-</style>
