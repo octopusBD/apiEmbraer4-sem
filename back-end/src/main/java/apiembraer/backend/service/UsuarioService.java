abc83ef5abc487e1eb441321ef0f0c40431c2db1
Aldrik Alvaro <aldrikfariaalvaro@gmail.com>
2023-04-21 13:13:37 -0300

    Usuario Listar

--- back-end/src/main/java/apiembraer/backend/service/UsuarioService.java
+++ back-end/src/main/java/apiembraer/backend/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package apiembraer.backend.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import apiembraer.backend.DTO.UsuarioDTO;
 import apiembraer.backend.entity.PermissaoEntity;
+import apiembraer.backend.entity.SampleEntity;
 import apiembraer.backend.entity.UsuarioEntity;
 import apiembraer.backend.repository.PermissaoRepository;
 import apiembraer.backend.repository.UsuarioRepository;
@@ -25,30 +27,36 @@ public class UsuarioService {
     @Autowired
     private PasswordEncoder encoder;
 
-    @PreAuthorize("hasRole('ADMIN')")
-	public UsuarioEntity atualizarUsuario(Integer idUsuario, UsuarioDTO usuarioNovo) {
-	    Optional<UsuarioEntity> optionalUsuario = usuarioRepository.findById(idUsuario);
-	    if (optionalUsuario.isPresent()) {
-	        UsuarioEntity usuario = optionalUsuario.get();
-	        usuario.setNomeUsuario(usuarioNovo.getNomeUsuario());
-	        usuario.setLoginUsuario(usuarioNovo.getLoginUsuario());
-	        usuario.setSenhaUsuario(usuarioNovo.getSenhaUsuario());
-	        usuario.setEmail(usuarioNovo.getEmail());
-
-	        Optional<PermissaoEntity> optionalPermissao = permissaoRepository.findByIdPermissao(usuarioNovo.getIdPermissao().getIdPermissao());
-	        if (optionalPermissao.isPresent()) {
-	            usuario.setIdPermissao(optionalPermissao.get());
-	        }
-
-	        return usuarioRepository.save(usuario);
-	    } else {
-	        return null;
-	    }
-	}
+	    //@PreAuthorize("hasRole('ADMIN')")
+		public UsuarioEntity atualizarUsuario(Integer idUsuario, UsuarioDTO usuarioNovo) {
+		    Optional<UsuarioEntity> optionalUsuario = usuarioRepository.findById(idUsuario);
+		    if (optionalUsuario.isPresent()) {
+		        UsuarioEntity usuario = optionalUsuario.get();
+		        usuario.setNomeUsuario(usuarioNovo.getNomeUsuario());
+		        usuario.setLoginUsuario(usuarioNovo.getLoginUsuario());
+		        usuario.setSenhaUsuario(usuarioNovo.getSenhaUsuario());
+		        usuario.setEmail(usuarioNovo.getEmail());
+	
+		        Optional<PermissaoEntity> optionalPermissao = permissaoRepository.findByIdPermissao(usuarioNovo.getIdPermissao().getIdPermissao());
+		        if (optionalPermissao.isPresent()) {
+		            usuario.setIdPermissao(optionalPermissao.get());
+		        }
+	
+		        return usuarioRepository.save(usuario);
+		    } else {
+		        return null;
+		    }
+		}
 
 
 	   public void deleteById(Integer idUsuario) {
 		   usuarioRepository.deleteById(idUsuario);
 	    }
+	   
+	    // LISTAR //
+	    public List<UsuarioEntity> getAllUsuarios() {
+	        return usuarioRepository.findAll();
+	    }
+	   
 	}
 
