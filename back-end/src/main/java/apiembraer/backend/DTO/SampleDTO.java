549ed982a666ef6b91be6f8eef80c54c49aa84ba
Aldrik Alvaro <aldrikfariaalvaro@gmail.com>
2023-05-12 20:36:19 -0300

    ajustes editor

---
+++ back-end/src/main/java/apiembraer/backend/DTO/SampleDTO.java
@@ -0,0 +1,12 @@
+package apiembraer.backend.DTO;
+
+import apiembraer.backend.entity.PermissaoEntity;
+import lombok.Data;
+
+@Data
+public class SampleDTO {
+
+	private Integer idSample;
+	private String statusSample;
+	
+}
